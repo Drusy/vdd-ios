3dcdccc2d618fa50d2427ab9e781adee132eb177
Drusy <kevin.renella@gmail.com>
2019-10-15 21:05:49 +0200

    Screenshots iPhone 11 Pro Max

--- VolantDesDomes/Classes/Utils/StyleManager.swift
+++ VolantDesDomes/Classes/Utils/StyleManager.swift
@@ -26,6 +26,14 @@ class StyleManager {
         UINavigationBar.appearance().tintColor = tintColor
         UIBarButtonItem.appearance().setTitleTextAttributes(attributes, for: .normal)
         UITabBar.appearance().tintColor = tintColor
+        
+        // Navigation bar iOS 13 fix
+        // https://sarunw.com/posts/uinavigationbar-changes-in-ios13/
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithDefaultBackground()
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
     }
     
     lazy var tintColor: UIColor = {

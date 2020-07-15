0a969f741761423df074c324625c520a37eb01ce
Drusy <kevin.renella@gmail.com>
2020-07-15 20:30:08 +0200

    Delete Carthage/Checkout content

--- Carthage/Checkouts/ObjectMapperAdditions/podPush.command
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-base_dir=$(dirname "$0")
-cd "$base_dir"
-
-# Pushing latest version to cocoapods
-pod trunk push --allow-warnings
--- Carthage/Checkouts/Siren/Package.swift
+++
@@ -1,8 +0,0 @@
-// swift-tools-version:5.1
-import PackageDescription
-
-let package = Package(
-    name: "Siren",
-    products: [.library(name: "Siren", targets: ["Siren"])],
-    targets: [.target(name: "Siren", path: "Sources")]
-)
